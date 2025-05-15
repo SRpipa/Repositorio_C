46e573036a74bc1b5abfca07acf6b6a57c07fa86
Piparron_ubuntu <adriangomezrey05@gmail.com>
2025-05-15 12:57:48 +0200

    practica 4, prueba

---
+++ Practica_4/palabras.c
@@ -0,0 +1,13 @@
+
+
+//modulo palabras.h
+//#ifndef ALD_PALABRAS_H
+//#define ALD_PALABRAS_H
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    printf("palabras.h\n");
+    return 0;
+}
